84659e509ab325a7d425faf9214fb5edb8b5973f
s-muramoto <smuramoto_nes@nec.com>
2021-02-19 10:43:15 +0900

    secret key os.env change

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
-from .settings_local import *
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/W
